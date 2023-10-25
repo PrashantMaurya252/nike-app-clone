d11f29f04012d7a33a763aa0bd8a8dca33478032
PrashantMaurya252 <mauryaprashant252@gmail.com>
2023-10-25 12:18:46 +0530

    Offer and Review Section Completed

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,10 @@
 
 
-function Button({label,iconURL}) {
+function Button({label,iconURL,backgroundColor,textColor,borderColor}) {
   return (
-    <button className="flex justify-center items-center gap-2 px-7 py-7 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">{label}
-    <img src={iconURL} alt="arrow right icon" className="ml-2 rounded-full w-5 h-5"/>
+    <button className={`flex justify-center items-center gap-2 px-7 py-7 border font-montserrat text-lg leading-none
+     ${backgroundColor ?`${backgroundColor}${textColor}${borderColor}`:'bg-coral-red  text-white border-coral-red'} rounded-full w-full'}`}>{label}
+    {iconURL &&<img src={iconURL} alt="arrow right icon" className="ml-2 rounded-full w-5 h-5"/>}
     
     </button>
   )
